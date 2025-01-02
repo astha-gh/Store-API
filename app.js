1b6127fd500f67846fd5b5885115f8da90c32d84
astha-gh <asthadevadiga7@gmail.com>
2025-01-02 10:37:16 +0530

    Made error handler and not found middleware

--- app.js
+++ app.js
@@ -3,9 +3,14 @@ const express = require('express');
 const app = express();
 const productRoute = require('./router/products');
 const {connectDB} = require('./dB/connect');
+const errorHandler = require('./middleware/error-handler');
+const notFound = require('./middleware/non-found');
 
 app.use('/api/v1/products' , productRoute);
 app.use(express.json());
+
+app.use(errorHandler);
+app.use(notFound);
 
 
 const start = async() => {
