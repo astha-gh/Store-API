1103daf4a38e2599acdb82cd2e9e8507a10d8eff
astha-gh <asthadevadiga7@gmail.com>
2025-01-02 10:24:12 +0530

    Connected with DB

--- app.js
+++ app.js
@@ -2,11 +2,23 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const productRoute = require('./router/products');
+const {connectDB} = require('./dB/connect');
 
 app.use('/api/v1/products' , productRoute);
 app.use(express.json());
 
 
-app.listen(3017 , () => {
-    console.log("Listening at port 3017...");
-})
+const start = async() => {
+    try{
+        await connectDB(process.env.MONGO_URI);
+        console.log("Connected with DB");
+        app.listen(3017 , () => {
+            console.log("Listening at port 3017...");
+        })
+    }
+    catch(err){
+        console.log(err);
+    }
+}
+
+start();
